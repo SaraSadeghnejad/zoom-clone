92e00b3fd8e2ddb89ea997b5eb9eba298100a51a
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2024-04-04 08:00:37 +0330

    create some folders

---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <section className="max-sm:hidden lg:w-[264px] sticky top-0 left-0 h-screen w-fit bg-dark-1 flex flex-col justify-between p-6 pt-28 text-white">
+      <div className="flex flex-col gap-6"></div>Sidebar
+    </section>
+  );
+};
+
+export default Sidebar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ const config = {
       colors: {
         dark:{
           1:'#1C1F2E',
-          2:''
+          2:'#161925'
         },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
